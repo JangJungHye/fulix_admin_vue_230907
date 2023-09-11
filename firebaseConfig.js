08f67c5c2dab9759ea20415d8df358bf81e0800f
Junghye Jang <jessica@futuresky.co.kr>
2023-09-11 20:29:10 +0900

    file추가

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyCO_2Aoki5drWwBMIuNdMJiYaU5nQ_7q94',
+  authDomain: 'fulixadminvf2.firebaseapp.com',
+  databaseURL: 'https://fulixadminvf2-default-rtdb.firebaseio.com',
+  projectId: 'fulixadminvf2',
+  storageBucket: 'fulixadminvf2.appspot.com',
+  messagingSenderId: '477511337483',
+  appId: '1:477511337483:web:f3cde3f2db6fb58469690e'
+}
